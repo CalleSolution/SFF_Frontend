88c0107a97693444bd59099c422af7ad0fdc1b50
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2020-05-18 10:33:56 +0200

    update1.1

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+fetchMovies();
 
-document.getElementById("demo").innerHTML = "Hello World!";
+var moviesList = document.getElementById("moviesList")
+
+
+function fetchMovies(){
+    fetch("https://localhost:44361/api/film")
+    .then(function(response){
+                return response.json();
+    })
+    .then(function(json){
+
+        console.log("fetchMovies",json);
+
+        for(i=0; i< json.length; i++) {
+            console.log(json[i].name)
+            moviesList.insertAdjacentHTML("beforeend",json[i].name)
+        }
+    })
+}
