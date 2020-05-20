99e04260325ab0447b9306b808978767962d0fde
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2020-05-20 14:25:07 +0200

    added more things

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 //Hämta alla filmer vid uppstart av sidan
 fetchMovies();
+fetchTrivia();
+
 //Skapa en lista av movies
 var moviesList = document.getElementById("flex-container");
+
 
 //Logga in på sitt konto
 var loginButton = document.getElementById("login");
 
 function Login()
 {
+    fetchMovies();
     console.log("Inloggningsläget");
     loginButton.innerHTML="Logga in";
     loginButton.insertAdjacentHTML("beforeend", 
@@ -16,13 +20,14 @@ function Login()
         var userLogin = document.getElementById("userName").value;
         var userPassword = document.getElementById("userPass").value;
         
-        fetchStudios(userLogin,userPassword)
+        fetchStudios(userLogin,userPassword);
     });
 }
 //Registrera studio
 var registerButton = document.getElementById("login");
 function RegStudio() 
 {
+    fetchMovies();
         console.log("Registreringsläget");
         registerButton.innerHTML = "Registrera ny studio";
         registerButton.insertAdjacentHTML("beforeend",  
@@ -33,7 +38,12 @@ function RegStudio()
         var password = document.getElementById("userPass").value;
         addStudio(studio,password,true);
         console.log(studio,password,true);
+        login.innerHTML="";
+        login.insertAdjacentHTML("beforeend",  
+        "<br><div>Du har registrerat en ny filmstudio! <button id='loginButton' onclick='Login()'>Logga in</button> <button onclick='RegStudio()'>Registrera Studio</button></div>");
+        fetchMovies();
         });
+
 }
 //Funktion för att hämta alla filmer
 function fetchMovies(){
@@ -41,13 +51,29 @@ function fetchMovies(){
     .then(function(response){
                 return response.json();
     })
-    .then(function(json){
+    .then(function(movie){
 
-        console.log("fetchMovies",json);
+        console.log("fetchMovies",movie);
         moviesList.innerHTML = "";
-        for(i=0; i< json.length; i++) {
-            console.log(json[i].name)
-            moviesList.insertAdjacentHTML("beforeend","<div class=movie-container>" + json[i].name + "</div>")
+        for(i=0; i< movie.length; i++) {
+            console.log(movie[i].name)
+            moviesList.insertAdjacentHTML("beforeend","<div class=movie-container>" + movie[i].name + "</div>")
+        }
+    })
+}
+
+//Funktion för att hämta alla trivia
+function fetchTrivia(){
+    fetch("https://localhost:44361/api/filmtrivia")
+    .then(function(response){
+                return response.json();
+    })
+    .then(function(trivia){
+
+        console.log("fetchTrivia",trivia);
+        
+        for(i=0; i< trivia.length; i++) {
+            console.log(trivia[i].id)
         }
     })
 }
@@ -77,6 +103,7 @@ function addStudio(name, password, verified){
 }   
 
 //Funktion för att hämta alla Studios --- Skriver ut allting just nu
+var logutButton = document.getElementById("logout");
 function fetchStudios(userName,userPass)
 {
     fetch("https://localhost:44361/api/filmstudio")
@@ -86,18 +113,49 @@ function fetchStudios(userName,userPass)
     .then(function(json){
 
         console.log("fetchStudios",json);
-        moviesList.innerHTML = "";
-        for(i=0; i< json.length; i++) 
-        {
-            if(userName == json[i].name && userPass == json[i].password)
-            {
+        
+        var result = json.find(a => a.name == userName && a.password == userPass);
+        if (result === true)
+        {    
+                login.innerHTML="";
                 console.log("Du loggade in!")
-            }
-            else
-            {
-                console.log("någonting gick fel!");
-            }
-           
+                localStorage.setItem("userId",userName);
+                login.insertAdjacentHTML("beforeend", 
+                "<br><div>Du är inloggad som " + json[i].name + "<button id='logout'>Logga ut</button></div>")
+                console.log(localStorage.getItem("userId"));
+                fetchMovies();
+                
+                logout.addEventListener("click", function()
+                        {
+                            login.innerHTML="";
+                            login.insertAdjacentHTML("beforeend",  
+                            "<br><div>Du är utloggad! <button id='loginButton' onclick='Login()'>Logga in</button> <button onclick='RegStudio()'>Registrera Studio</button></div>");
+                            fetchMovies();
+                        });        
         }
+        // for(i=0; i< json.length; i++) 
+        // {
+        //     console.log(result);
+        //     if(userName == json[i].name && userPass == json[i].password)
+        //     {
+        //         login.innerHTML="";
+        //         console.log("Du loggade in!")
+        //         localStorage.setItem("userId",userName);
+        //         login.insertAdjacentHTML("beforeend", 
+        //         "<br><div>Du är inloggad som " + json[i].name + "<button id='logout'>Logga ut</button></div>")
+        //         console.log(localStorage.getItem("userId"));
+        //         fetchMovies();
+                
+        //         logout.addEventListener("click", function()
+        //                 {
+        //                     login.innerHTML="";
+        //                     login.insertAdjacentHTML("beforeend",  
+        //                     "<br><div>Du är utloggad! <button id='loginButton' onclick='Login()'>Logga in</button> <button onclick='RegStudio()'>Registrera Studio</button></div>");
+        //                     fetchMovies();
+        //                 });
+        //     }  
+        // }
     });
+
+    
 }
